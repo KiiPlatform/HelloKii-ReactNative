c9abc357db425981547185cf8ead34e79a67d098
rizakii <riza.alaudin@kii.com>
2016-07-20 16:45:40 +0900

    add default KiiSDK.js

--- HelloKii/index.ios.js
+++ HelloKii/index.ios.js
@@ -5,6 +5,8 @@
  */
 
 import React, { Component } from 'react';
+var kii = require('./KiiSDK.js').create();
+kii.Kii.initializeWithSite("___APPID___", "___APPKEY___", kii.KiiSite.US);
 import {
   AppRegistry,
   StyleSheet,
